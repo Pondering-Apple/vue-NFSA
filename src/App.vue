bf2c1776196c409f58e3fb7edb7e4487b05352be
Pondering-Apple <136867740+Pondering-Apple@users.noreply.github.com>
2024-09-22 19:57:06 +1000

    learning vue

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,90 @@
-<template></template>
+<template>
+  <h2 v-bind:id="headingId">Heading</h2>
+  <button v-bind:disabled="isDisabled">Bind</button>
+  <h2 class="underline">underlined text</h2>
+  <h2 class="underline" v-bind:class="status">Status</h2>
+  <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+  <h2 v-bind:class="isSoldOut ? 'sold-out' : 'new'">Soldout?</h2>
+  <h2 v-bind:class="['new', 'promoted']">Newly promoted Movie</h2>
+  <h2 v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']">
+    Array Conditional Movie
+  </h2>
+  <h2
+    v-bind:class="{
+      promoted: isPromoted,
+      new: !isSoldOut,
+      'sold-out': isSoldOut
+    }"
+  >
+    Object Conditional Movie
+  </h2>
+
+  <h2
+    v-bind:style="{
+      color: highlightColor,
+      'font-size': headerSize + 'px',
+      padding: '20px'
+    }"
+  >
+    Inline Style
+  </h2>
+  <h2 v-bind:style="headerStyleObject">Style Object</h2>
+  <div v-bind:style="[baseStyleObject, successStyleObject]">Success Style</div>
+  <div v-bind:style="[baseStyleObject, dangerStyleObject]">Danger Style</div>
+</template>
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      greet: 'Hello',
+      name: 'batman',
+      channel: '<b>Codevolution</b>',
+      headingId: 'heading',
+      isDisabled: false,
+      status: 'danger',
+      isPromoted: true,
+      isSoldOut: true,
+      highlightColor: 'orange',
+      headerSize: 50,
+      headerStyleObject: {
+        color: 'orange',
+        fontSize: '50px',
+        padding: '20px'
+      },
+      baseStyleObject: {
+        fontSize: '50px',
+        padding: '10px'
+      },
+      successStyleObject: {
+        color: 'green',
+        backgroundColor: 'lightgreen',
+        border: '1px solid green'
+      },
+      dangerStyleObject: {
+        color: 'darkred',
+        backgroundColor: 'red',
+        border: '1px solid darkred'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.underline {
+  text-decoration: underline;
+}
+
+.promoted {
+  font-style: italic;
+}
+
+.new {
+  color: green;
+}
+
+.sold-out {
+  color: red;
+}
+</style>
