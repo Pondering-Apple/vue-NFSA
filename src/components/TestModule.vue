d1177b5b831c850ca828d98e29c5ad940eda9b91
Pondering-Apple <136867740+Pondering-Apple@users.noreply.github.com>
2024-10-02 16:07:46 +1000

    I got the map thing to work

--- src/components/TestModule.vue
+++ src/components/TestModule.vue
@@ -1,10 +1,27 @@
 <template>
   <h2>Test Module</h2>
+  <WorldMap :fill="fillColor" :stroke="strokeColor" @country-click="onCountryClick" />
 </template>
 
 <script>
+import WorldMap from 'world-map-vue'
+
 export default {
-  name: 'TestModule'
+  name: 'TestModule',
+  components: {
+    WorldMap
+  },
+  data() {
+    return {
+      fillColor: 'red',
+      strokeColor: 'black'
+    }
+  },
+  methods: {
+    onCountryClick(countryCode) {
+      alert(`Country clicked: ${countryCode}`)
+    }
+  }
 }
 </script>
 
