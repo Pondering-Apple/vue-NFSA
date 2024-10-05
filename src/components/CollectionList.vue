9f0bb041f877a827c8f750afb221913c4e8faf2c
Pondering-Apple <136867740+Pondering-Apple@users.noreply.github.com>
2024-10-05 12:58:38 +1000

    just had to clear up an empty thing

--- src/components/CollectionList.vue
+++ src/components/CollectionList.vue
@@ -1,16 +1,5 @@
 <template>
-  <!-- <div class="collectionContainer">
-    <div class="itemContainer">
-      <div class="imagePlaceHolder"></div>
-      <div class="itemInformation">
-        <h3>Item Title</h3>
-        <div class="itemLink">
-          <p>Start of Description</p>
-          <p><strong>Click for more...</strong></p>
-        </div>
-      </div>
-    </div>
-  </div> -->
+  <span> </span>
 </template>
 
 <script>
@@ -19,41 +8,4 @@ export default {
 }
 </script>
 
-<style>
-/* .collectionContainer {
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-}
-.itemContainer {
-  width: 80%;
-  height: 20vh;
-  display: flex;
-  padding: 20px;
-  background-color: green;
-  border-left: 5px solid black;
-  border-right: 5px solid black;
-  box-sizing: border-box;
-}
-.imagePlaceHolder {
-  height: 100%;
-  width: 25vh;
-  margin-right: 7%;
-  display: inline-block;
-  background-color: yellow;
-}
-.itemInformation {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  width: 90%;
-  box-sizing: border-box;
-}
-.itemLink {
-  width: 80%;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-} */
-</style>
+<style></style>
